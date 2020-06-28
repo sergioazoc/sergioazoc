ef7515261f7199296c7ca0a25937e805cf2b4a5a
Sergio Azócar <sergioazoc@gmail.com>
2020-06-28 00:41:46 -0400

    edit header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <b-navbar variant="transparent" type="dark">
-      <b-navbar-brand href="#">
+      <b-navbar-brand to="/">
         {S}
       </b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <b-nav-item to="/">
           Inicio
+        </b-nav-item>
+        <b-nav-item to="/about">
+          Sobre mi
         </b-nav-item>
         <b-nav-item to="/portfolio">
           Portafolio
@@ -14,13 +17,23 @@
       </b-navbar-nav>
     </b-navbar>
     <b-aspect aspect="16:9" tag="header" class="header">
-      <b-container class="header__cover d-flex justify-content-center text-center flex-column text-light" fluid>
-        <h1 class="display-4">
-          Sergio Azócar
-        </h1>
-        <p class="lead">
-          Frontend Developer
-        </p>
+      <b-container class="header__cover d-flex justify-content-center align-items-center text-light flex-column flex-sm-row" fluid>
+        <b-img
+          class="header__img border border-primary"
+          src="../assets/img/sergio-azocar.jpg"
+          height="130px"
+          width="130px"
+          rounded="circle"
+          alt="Circle image"
+        />
+        <div class="ml-4">
+          <h1 class="header__title display-4">
+            Sergio Azócar
+          </h1>
+          <p class="header__subtitle lead">
+            Frontend Developer
+          </p>
+        </div>
       </b-container>
     </b-aspect>
     <Nuxt />
@@ -36,7 +49,7 @@
 }
 .header{
   min-height: 50vh;
-  max-height: 72vh;
+  max-height: 65vh;
   background-image: url("../assets/img/fondo-header-sergioazoc.jpg");
   background-size: cover;
   background-position: 50% 38%;
@@ -47,6 +60,18 @@
   $body-bg: #250657;
   height: 100%;
   background: $color linear-gradient(0deg, $body-bg, $color) repeat-x;
+}
+@media (max-width: 575.98px){
+  .header__img{
+    height: 100px;
+    width: 100px;
+  }
+  .header__title{
+    font-size: 2.5rem;
+  }
+  .header__subtitle{
+    text-align: center;
+  }
 }
 .footer{
   background-color: #250657;
---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-container fluid="sm" tag="section">
+    <h2>Sobre mi</h2>
+    <p class="lead">
+      Este año retomé la programación y me especialicé en frontend, trabajando con vue.js y javascript en general. Tengo buen dominio de HTML/CSS y algunos frameworks de css como bootstrap o materialize.
+    </p>
+    <p class="lead">
+      Entre el 2016 y el 2019 emprendí, estuve dedicado al marketing digital, dónde aprendí bastante sobre branding y marketing en general (SEO, social media, inbound, etc.), esto gracias a los muchos cursos que hice (<a href="https://platzi.com/@sergioazocar/" target="_blank" rel="noopener noreferrer">Platzi</a>) y el trabajo con clientes en mi empresa. Durante ese tiempo implementé diversos sitios web con wordpress y tiendas con woocommerce, además de algunos desarrollos con PHP y Ruby on Rails. También estrategias de marketing y branding para empresas.
+    </p>
+    <p class="lead">
+      Así como me gusta aprender, también enseñar, lo que me llevó a realizar charlas, seminarios, cursos y mentorías por toda la región de Los Lagos y algunas ciudades de la región de Los Ríos.
+    </p>
+  </b-container>
+</template>
