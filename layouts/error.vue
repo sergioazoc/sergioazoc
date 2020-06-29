2cb151b2ddec66a07672df33ef2aa6aa393cea11
Sergio Azócar <sergioazoc@gmail.com>
2020-06-29 02:40:13 -0400

    update error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Error 404.</h1>
+  <div class="container py-5 text-center">
+    <h1 class="display-4">
+      Error 404
+    </h1>
+    <h2>
+      No se que estás buscando, pero no lo encontramos... 🤷‍♂️
+    </h2>
+    <p class="lead">
+      Si quieres puedes buscar un Pokémon en mi <a href="https://pokedex-vue.now.sh/" target="_blank" rel="noopener noreferrer">Pokedex-Vue!</a>
+    </p>
+  </div>
 </template>
